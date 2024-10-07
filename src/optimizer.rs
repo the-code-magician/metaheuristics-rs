d78309809d95a675dfac8984aa8236970492b0e3
João Cardoso <jooaaoo@gmail.com>
2024-10-07 20:05:30 +0200

    Update optimizer.rs

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -1,8 +1,11 @@
+use crate::archive::Archive;
+
 pub trait Optimizer {
     type Solution;
     type Fitness;
 
-    fn optimize<F>(&self, fitness_function: F) -> Self::Solution
+    fn optimize<F, A>(&self, fitness_function: F, archive: &mut A)
     where
-        F: Fn(&Self::Solution) -> Self::Fitness;
+        F: Fn(&Self::Solution) -> Self::Fitness,
+        A: Archive<Solution = Self::Solution, Fitness = Self::Fitness>;
 }
