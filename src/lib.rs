ba2ac81769402ed9a623a5a73253551e56841eec
João Cardoso <jooaaoo@gmail.com>
2024-10-07 20:04:55 +0200

    add lib

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub mod optimizer;
+pub use optimizer::Optimizer;
+
+pub mod archive;
+pub use archive::{Archive, BasicArchive};
+
+pub mod genetic_algorithm;
+pub mod simulated_annealing;
+pub mod particle_swarm;
+pub mod ant_colony;
