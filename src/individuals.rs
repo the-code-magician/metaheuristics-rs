f3f4fad3959943312eab1cdca4df2a88baee0055
João Cardoso <jooaaoo@gmail.com>
2024-10-09 16:42:26 +0000

    fix tests 15

--- src/individuals.rs
+++ src/individuals.rs
@@ -40,9 +40,9 @@ impl Default for NumericIndividual {
 }
 
 impl Neighbor for NumericIndividual {
-    fn neighbor(&mut self) -> Self {
+    fn neighbor(&mut self, rng: &mut ThreadRng) -> Self {
         let mut rng = thread_rng();
-        let genes = genes.map(|v| v + rng.gen_range(-1.0..1.0)).collect();
+        let genes = self.genes.map(|v| v + rng.gen_range(-1.0..1.0)).collect();
         Self { genes }
     }
 }
